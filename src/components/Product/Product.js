916a9a7bd9480f208f5b55d1961ba75d305e0aed
jose cruz <cruzjosenyc@gmail.com>
2019-02-04 12:13:22 -0500

    rendered a name price and description in product component

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,9 +1,12 @@
 import React from "react"
+import "Product.css"
 
 const Product = () => {
   return(
-    <div>
-      <h1>This is product component</h1>
+    <div className = "product">
+      <h2>Name: name of product</h2>
+      <h2>Price: $5.00</h2>
+      <h3>Description: this is blah blah blah blah</h3>
     </div>
   )
 }
