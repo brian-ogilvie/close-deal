072e6bc11df327b6f17a49f458acb3326e8c1715
Brian Ogilvie <brian@brianogilviedesigns.com>
2019-02-04 11:48:25 -0500

    created Header with h1 and Link to /products

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import './Header.css'
+import {Link} from 'react-router-dom'
 
 class Header extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      active: ''
+    }
   }
 
   render() {
     return (
-      <h1>Header</h1>
+      <div className="Header">
+        <h1>Shopping App</h1>
+        <nav>
+          <ul className="Header__nav">
+            <li className="Header__nav-item"><Link className="Header__nav-link" to="/products">Products for Sale</Link></li>
+          </ul>
+        </nav>
+      </div>
     )
   }
 }
