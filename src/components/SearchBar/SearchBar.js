91d121125fd99b8d9dc708d6f510a28d3f05efeb
Brian Ogilvie <brian@brianogilviedesigns.com>
2019-02-04 15:11:55 -0500

    SearchBar component to filter Products in ProductsList

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './SearchBar.css'
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+    this.onFormSubmit = this.onFormSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  onFormSubmit(e) {
+    e.preventDefault()
+  }
+
+  async handleChange(e) {
+    const searchTerm = e.target.value
+    this.props.filterProducts(searchTerm)
+  }
+
+  render() {
+    return (
+      <form className="SearchBar" onSubmit={this.onFormSubmit}>
+        <input type="text" placeholder="Search Products" value={this.state.searchTerm} onChange={this.handleChange} />
+      </form>
+    )
+  }
+}
+
+export default SearchBar
