1af3b34e3761ecd4856970bea38667c6fad85238
Brian Ogilvie <brian@brianogilviedesigns.com>
2019-02-04 14:34:47 -0500

    added links to Header, added catch all / route in app.js that redirects to /products. Fixed Product.css

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.css'
 
 import Header from '../Header/Header'
@@ -12,7 +12,13 @@ class App extends Component {
         <Header />
         <main>
           <Switch>
+            <Route path='/' exact render={() => {
+              return <Redirect to='/products' />
+            }} />
             <Route path='/products' component={ ProductsList }/>
+            <Route path='/sell' component={ ProductsList }/>
+            <Route path='/profile' component={ ProductsList }/>
+            <Route path='/login' component={ ProductsList }/>
           </Switch>
         </main>
       </div>
