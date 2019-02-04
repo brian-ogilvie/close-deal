76050adfcbae6b8defa91404b047217099417921
Brian Ogilvie <brian@brianogilviedesigns.com>
2019-02-04 11:00:54 -0500

    created blank Header component

--- src/App/App.js
+++ src/App/App.js
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 
 // import HomePage from '../HomePage';
 // import Product from '../Product';
+import Header from '../Header/Header'
 
 class App extends Component {
   render() {
     return (
       <div className="container">
+        <Header />
         /*<Switch>
           <Route path='/' exact component={ HomePage }/>
           <Route path='/product' component={ Product }/>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <h1>Header</h1>
+    )
+  }
+}
+
+export default Header
