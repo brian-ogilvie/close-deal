9f53cbacf437f3fa97ea0dec7496fbb73472ca1c
Daniel Kim <daniel.seungchul.kim@gmail.com>
2019-02-04 11:39:24 -0500

    updated server

--- server.js
+++ server.js
@@ -7,6 +7,38 @@ app.get('/', (req, res) => {
   res.send('Welcome to the shopping app!')
 })
 
+app.get('/products', async(req,res) => {
+	try{
+		const products = await Product.findAll({raw:true})
+		res.json({products})
+	} catch(e){
+		res.status(500).json({message: e.message})
+	}
+})
+
+app.get('/products/:id', async (req, res) => {
+  try {
+    const product = await Product.findByPk(req.params.id, {raw:true})
+    if (!product) throw Error('Product not found!')
+    res.json({ product })
+  } catch(e) {
+    res.status(500).json({
+      message: e.message
+    })
+  }
+})
+
+app.post('/products', async (req, res) => {
+  console.log(req.body)
+  try {
+    const product = await Product.create(req.body);
+    res.json(product);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ message: e.message});
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Express server is listening on port ${PORT}`)
 })
