fea7eedf74ba227d18a46010d396a061c05ff3e9
Brian Ogilvie <brian@brianogilviedesigns.com>
2019-02-04 12:31:29 -0500

    axios call for productslist

--- src/components/ProductsList/ProductsList.js
+++ src/components/ProductsList/ProductsList.js
@@ -1,11 +1,38 @@
 import React from 'react'
+import axios from 'axios'
+import Product from '../Product/Product'
 
-// import Product from '../Product/Product'
+class ProductsList extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      products: [],
+    }
+    this.getProducts = this.getProducts.bind(this)
+  }
 
-const ProductsList = () => {
-	return (
-	    <div className="ProductsList"><h1>ProductsList</h1></div>
-	)
+  async getProducts() {
+    const res = await axios.get('/products')
+    const products = res.data
+    this.setState({products})
+  }
+
+  componentDidMount() {
+    this.getProducts()
+  }
+  render() {
+    const products = this.state.products.map((product, i) => {
+      return <Product key={i} product={product} />
+    })
+  	return (
+  	    <div className="ProductsList">
+          <h1>Products List</h1>
+          <div className="ProductsList__list">
+            {products}
+          </div>
+        </div>
+  	)
+  }
 } 
 
 export default ProductsList
