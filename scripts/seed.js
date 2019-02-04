6212eb11c55ee05d46f438627237e56261544c7c
Brian Ogilvie <brian@brianogilviedesigns.com>
2019-02-04 10:27:00 -0500

    began seed data

--- scripts/seed.js
+++ scripts/seed.js
@@ -7,6 +7,14 @@ const seed = async () => {
     await Transaction.truncate({cascade: true, restartIdentity:true})
     await Review.truncate({restartIdentity: true})
 
+    await User.bulkCreate([
+      {email: 'brian@brianogilviedesigns.com', first_name: 'Brian', last_name: 'Ogilvie', password: 'p0w3rt3@mRulez'},
+
+    ], {
+      validate: true,
+      individualHooks: true
+    })
+
     console.log('🗂  database seed complete')
   } catch (e) {
     console.error('‼️ ', e.message)
