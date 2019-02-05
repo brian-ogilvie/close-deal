c2550fdbf735c2f2e25e1330bc250a5ea6d21218
jose cruz <cruzjosenyc@gmail.com>
2019-02-05 11:38:40 -0500

    removed a tags styling

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,24 +1,27 @@
 import React from "react"
 import "./Product.css"
+import { Link } from "react-router-dom"
 
 const Product = ({product}) => {
-
+console.log(product);
 const imageInlineStyle = {backgroundImage:`url(${product.image_url})`}
 
   return(
-    <div className="Product">
-      <div className="Product__img-wrapper" style={imageInlineStyle}>
-        <img className="Product__image"  src={product.image_url} alt = {product.name} />
-      </div>
-      <div className="Product__info-wrapper">
-        <div className="Product__name-wrapper">
-          <h4>{product.name}</h4>
+      <div className="Product">
+        <Link to={`/products/${product.id}`}>
+          <div className="Product__img-wrapper" style={imageInlineStyle}>
+            <img className="Product__image"  src={product.image_url} alt = {product.name} />
+          </div>
+          <div className="Product__info-wrapper">
+            <div className="Product__name-wrapper">
+              <h4>{product.name}</h4>
+            </div>
+            <div className="Product__price-wrapper">
+              <h4>${product.price}</h4>
+            </div>
         </div>
-        <div className="Product__price-wrapper">
-          <h4>${product.price}</h4>
-        </div>
+        </Link>
       </div>
-    </div>
   )
 }
 
