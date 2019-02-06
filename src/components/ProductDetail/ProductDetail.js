566f6a195556c884db77326a034336bc5565c40b
Daniel Kim <daniel.seungchul.kim@gmail.com>
2019-02-06 13:29:31 -0500

    able to delete product UNCONDITIONALLY

--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import "./ProductDetail.css"
 import Review from '../Review/Review'
+import {Redirect} from 'react-router-dom'
 
 class ProductDetail extends Component {
   constructor() {
     super()
     this.state = {
-      product: {}
+      product: {},
+      isDeleted: false
     }
     this.getData = this.getData.bind(this)
   }
@@ -30,12 +32,26 @@ class ProductDetail extends Component {
   }
 
   render() {
+
+    const onProductDelete = (product) => {
+      axios.delete(`/products/${product.id}`)
+        .then(res=>console.log(res.data, "Product deleted"))
+        .then(this.setState({isDeleted:true}))
+    }
+
     const reviews = this.state.product.reviews
+
     const productReviews = reviews && reviews.length > 0 ?reviews.map(review => {
       return <Review key={review.id} review={review}/>
     }): <div></div>
+
+    if (this.state.isDeleted === true) {
+      return <Redirect to={'/products/'} />;
+    }
+
     return(
       <div className="ProductDetail__container">
+        <button className="ProductDetail__deleteButton" onClick={()=> onProductDelete(this.state.product)}>Delete</button>
         <div className="ProductDetail__details-container">
           <div className="ProductDetail__image-wrapper">
             <img className="ProductDetail__image" src={this.state.product.image_url} alt={this.state.product.name}/>
