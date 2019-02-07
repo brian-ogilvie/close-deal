e2bf281835a5281b3f104766f72f64ffd6b8a33c
jose cruz <cruzjosenyc@gmail.com>
2019-02-07 11:19:54 -0500

    the proper dates to the post and the reviews, added moment as a dependacy, and ststyled the review and product details component

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './Review.css'
+const moment = require('moment')
 
 const Review = ({review}) => {
 	return(
 		<div className='Review__wrapper'>
-			{review.poster.first_name}
-			{review.created_at.slice(0,10)}
+			<div className="Review__top-wrapper">
+				<div className="Review__name">Posted by: {review.poster.first_name} {review.poster.last_name}</div>
+			<div className="Review__date">{moment(review.created_at).format('LLLL')}</div>
+		</div>
+			<hr/>
 			{review.comment}
 		</div>
 	)
