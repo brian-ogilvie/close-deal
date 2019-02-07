efffb384142930ec914f6af1994cd41ca55d33fd
Brian Ogilvie <brian@brianogilviedesigns.com>
2019-02-07 18:51:32 -0500

    removed extra space at top of ProductDetail

--- models/index.js
+++ models/index.js
@@ -3,7 +3,14 @@ const Op = Sequelize.Op
 const bcrypt = require('bcrypt')
 const saltRounds = 14
 
-const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5000/shopping_app_db', {
+// const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5000/shopping_app_db', {
+//   dialect: 'postgres',
+//   define: {
+//     underscored: true
+//   }
+// });
+
+const db = new Sequelize({
   dialect: 'postgres',
   define: {
     underscored: true
