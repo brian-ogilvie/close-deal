5058efeab3e2cb8e5c80684285ef214c2e29e1f6
Brian Ogilvie <brian@brianogilviedesigns.com>
2019-02-08 10:55:48 -0500

    fade in all components

--- models/index.js
+++ models/index.js
@@ -3,7 +3,8 @@ const Op = Sequelize.Op
 const bcrypt = require('bcrypt')
 const saltRounds = 14
 
-const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5000/shopping_app_db', {
+// const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5000/shopping_app_db', {
+  const db = new Sequelize({database: 'shopping_app_db',
   dialect: 'postgres',
   define: {
     underscored: true
